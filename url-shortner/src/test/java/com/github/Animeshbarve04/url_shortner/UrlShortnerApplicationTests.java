486dc43cb2ae1df4d7f3eba6378ffd7b872e1622
Animesh Barve <animeshcoding@gmail.com>
2026-07-14 23:41:25 +0530

    Project Structure

---
+++ url-shortner/src/test/java/com/github/Animeshbarve04/url_shortner/UrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Animeshbarve04.url_shortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
